311e209cc4099290fbc91a159be192e3a4358461
Teddy Jean-Francois <teddy.jean-francois@ibm.com>
2022-04-23 23:13:38 +0200

    feat : setup library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,19 @@
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
-  theme: {
-    height: {
-      betana_header: '10%',
-      betana_body: '90%',
-      betana_menu: '10%',
-      betana_bill: '20%',
-      betana_central_content: '70%',
-      betana_profile_icon: '60px',
+  theme: {    
+    extend: {
+      colors: {
+        betana_navy_blue: '#11182B',
+        betana_sky_blue: '#1C358E',
+        betana_grey: '#E1E1E1',
+        betana_red: '#EB0000',
+        betana_green: '#52BF41',
+        betana_yellow: '#FFD501',
+        betana_white: '#FFD501',
+      },
     },
-    colors: {
-      betana_navy_blue: '#11182B',
-      betana_sky_blue: '#1C358E',
-      betana_grey: '#E1E1E1',
-      betana_red: '#EB0000',
-      betana_green: '#52BF41',
-      betana_yellow: '#FFD501',
-      betana_white: '#FFD501',
-    },
-    extend: {},
   },
   plugins: [],
 }
